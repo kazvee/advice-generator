a24a02e07b407fb117f4c06c9193dc3496f1b406
karvok <kayvee123@gmail.com>
2023-08-30 10:12:45 -0600

    Display fetched advice on page using state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { useState } from "react";
+
 const App = () => {
+  const [advice, setAdvice] = useState("");
 
   const getAdvice = async () => {
     const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
-    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
   };
 
-  return (<div>
-    <h1>
-      Hello World! 🌍
-    </h1>
-    <button onClick={getAdvice}>Get Advice</button>
-  </div>
+  return (
+    <div>
+      <h1>
+        {advice}
+      </h1>
+      <button onClick={getAdvice}>Get Advice</button>
+    </div>
   );
 };
 
