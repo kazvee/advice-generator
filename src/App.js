ddd62858538f71b74c068176525fe65e0988666b
karvok <kayvee123@gmail.com>
2023-08-30 09:40:30 -0600

    Add API call & log response to the console

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 const App = () => {
+
+  const getAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    console.log(data.slip.advice);
+  };
+
   return (<div>
     <h1>
       Hello World! 🌍
     </h1>
-    <button>Get Advice</button>
+    <button onClick={getAdvice}>Get Advice</button>
   </div>
   );
 };
