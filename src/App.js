610a72d08e00c6fb52599dc6d69c56ca82c4e795
karvok <kayvee123@gmail.com>
2023-08-30 10:19:32 -0600

    Add counter to track number of times advice is received

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
   const getAdvice = async () => {
     const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
     setAdvice(data.slip.advice);
+
+    const newCount = count + 1;
+    setCount(newCount);
   };
 
   return (
@@ -15,6 +19,9 @@ const App = () => {
         {advice}
       </h1>
       <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        You have received advice <strong>{count}</strong> times! 😃
+      </p>
     </div>
   );
 };
